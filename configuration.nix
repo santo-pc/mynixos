d6e2eaa6c585f70c8caf6d7ab02f91a85b1a3fe6
santiago <sapalacio2@gmail.com>
2024-10-26 02:52:30 +0200

    adding basic nix config

--- configuration.nix
+++ configuration.nix
@@ -3,14 +3,10 @@
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
-### let
-###   home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-21.11.tar.gz";
-### in
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
-       #<home-manager/nixos>
     ];
 
   # Bootloader.
@@ -18,12 +14,7 @@
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking.hostName = "nixos"; 
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -50,10 +41,6 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
 
-  # Enable the GNOME Desktop Environment.
-  # services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
-
   # Configure keymap in X11
   services.xserver = {
     enable = true;
@@ -67,11 +54,6 @@
     #videoDrivers = ["amdgpu-pro"];
   };
 
-  programs.hyprland.enable = true;
- # programs.hyprland.package = inputs.hyprland.packages."${pkgs.system}".hyprland;
-
-   #wayland.windowManager.hyprland.enable = true; # enable Hyprland
-
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
@@ -79,9 +61,6 @@
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
-  #hardware.opengl.extraPackages = [
-  #  rocm-opencl-icd
-  #];
 
   security.rtkit.enable = true;
   services.pipewire = {
@@ -89,12 +68,6 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
 
@@ -109,40 +82,14 @@
     ];
   };
 
-  # List packages installed in system profile. To search, run:
   environment.systemPackages = with pkgs; [
     vim
     neovim
   ];
 
   virtualisation.docker.enable = true;
-
   nixpkgs.config.allowUnfree = true;
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
 
-  # List services that you want to enable:
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.11"; # Did you read the comment?
-
+  system.stateVersion = "22.11";
 }
