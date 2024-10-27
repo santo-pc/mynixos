5a891f46364d39719c228c1faf34fb7efb59b8a7
santiago <sapalacio2@gmail.com>
2024-10-27 21:50:52 +0100

    change structure with modules

--- flake.nix
+++ flake.nix
@@ -2,9 +2,8 @@
   description = "NixOS configuration";
 
   inputs = {
-   nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
-   nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
-    # home-manager, used for managing user configuration
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-22.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -13,50 +12,44 @@
   };
 
   outputs = { nixpkgs, nixpkgs-unstable, home-manager, hyprland, ... }@inputs:
-  let
+    let
     lib = nixpkgs.lib;
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-    pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
+  system = "x86_64-linux";
+  pkgs = nixpkgs.legacyPackages.${system};
+  pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
   in
   {
     nixosConfigurations = {
       nixos = lib.nixosSystem {
-        #system = "x86_64-linux";
-	inherit system;
+        inherit system;
         modules = [
           ./configuration.nix
 
-          # make home-manager as a module of nixos
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.san = import ./home.nix;
 
-            home-manager.users.san = import ./home.nix;
-
-            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
-          }
+            }
         ];
-	specialArgs = {
-		inherit pkgs-unstable;
+        specialArgs = {
+          inherit pkgs-unstable;
         };
 
       };
     };
 
     homeConfigurations."san@nixos" = home-manager.lib.homeManagerConfiguration {
-      		pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
 
-      	modules = [
-          {
-             wayland.windowManager.hyprland = {
-             enable = true;
-             # set the flake package
-             package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-          };
-        }
-        # ...
+      modules = [
+      {
+        wayland.windowManager.hyprland = {
+          enable = true;
+          package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+        };
+      }
       ];
     };
   };
--- home.nix
+++ home.nix
@@ -114,27 +114,13 @@
   programs.bash = {
     enable = true;
     enableCompletion = true;
-    # TODO add your custom bashrc here
-    bashrcExtra = ''
-      export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
-    '';
-
     # set some aliases, feel free to add more or remove some
     shellAliases = {
       k = "kubectl";
     };
   };
 
-  # This value determines the home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update home Manager without changing this value. See
-  # the home Manager release notes for a list of state version
-  # changes in each release.
+  programs.home-manager.enable = true;
   home.stateVersion = "22.11";
 
-  # Let home Manager install and manage itself.
-  programs.home-manager.enable = true;
 }
