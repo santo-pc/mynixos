dddc506c9d2978ab80731c2e6d5c3c65fc20663a
santiago <sapalacio2@gmail.com>
2024-10-27 22:04:40 +0100

    change structure with modules

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,9 +1,12 @@
 { pkgs, ... }:
 
 {
-  fonts.packages = with pkgs; [
+  fonts.fonts = with pkgs; [
     font-awesome 
     jetbrains-mono
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
 
     (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
   ];
