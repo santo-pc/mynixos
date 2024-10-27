2a4ba852024a73f0907ad5a3b4e6b815892da31d
santiago <sapalacio2@gmail.com>
2024-10-27 22:59:49 +0100

    change structure with modules

--- home.nix
+++ home.nix
@@ -114,7 +114,7 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    autosuggestions.enable = true;
+    autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
 
     shellAliases = {
