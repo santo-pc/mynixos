b1111154c730b9e04d6cb7191d0155fb06dc5c30
santiago <sapalacio2@gmail.com>
2024-10-27 17:24:11 +0100

    adding basic nix config

---
+++ modules/services.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+
+{
+  services = {
+
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+    dbus.enable = true;
+    picom.enable = true;
+    openssh.enable = true;
+    spice-vdagentd.enable = true;
+    xserver = {
+      enable = true;
+      layout = "us";
+      windowManager.i3.enable = true;
+
+      displayManager.defaultSession = "none+i3";
+      displayManager.lightdm.enable = true;
+      displayManager.autoLogin.enable = true;
+    };
+  };
+}
+
