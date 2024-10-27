f29bd8111c7d195b178242d2e97e00f9ce1e7218
santiago <sapalacio2@gmail.com>
2024-10-27 21:55:16 +0100

    change structure with modules

--- modules/default.nix
+++ modules/default.nix
@@ -6,9 +6,11 @@
         ./fonts.nix
         ./internationalisation.nix
         ./networking.nix
+        ./nixsettings.nix
         ./packages.nix.nix
         ./services.nix
         ./sound.nix
         ./users.nix
+        ./virtualisation.nix
     ];
 }
