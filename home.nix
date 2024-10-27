739d58a99f41b48b3760996a29a31e633af1166c
santiago <sapalacio2@gmail.com>
2024-10-27 22:30:34 +0100

    change structure with modules

--- home.nix
+++ home.nix
@@ -110,26 +110,27 @@
       selection.save_to_clipboard = true;
     };
   };
+
   programs.zsh = {
     enable = true;
-    enableCompletion = true;
-    # autosuggestion.enable = true;
-    enableSyntaxHighlighting.enable = true;
-
-    shellAliases = {
-      ll = "ls -l";
-      ls = "ls -la";
-      update = "sudo nixos-rebuild switch";
-    };
-    history = {
-      size = 10000;
-      path = "${config.xdg.dataHome}/zsh/history";
-    };
-    oh-my-zsh = {
-      enable = true;
-      plugins = [ "git" "thefuck" ];
-      theme = "robbyrussell";
-    };
+    # enableCompletion = true;
+    # # autosuggestion.enable = true;
+    # enableSyntaxHighlighting.enable = true;
+    #
+    # shellAliases = {
+    #   ll = "ls -l";
+    #   ls = "ls -la";
+    #   update = "sudo nixos-rebuild switch";
+    # };
+    # history = {
+    #   size = 10000;
+    #   path = "${config.xdg.dataHome}/zsh/history";
+    # };
+    # oh-my-zsh = {
+    #   enable = true;
+    #   plugins = [ "git" "thefuck" ];
+    #   theme = "robbyrussell";
+    # };
 
   };
 
