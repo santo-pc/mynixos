e4060aa738f4ed0d28e8d63a7e82bdfbb7e57fc4
santiago <sapalacio2@gmail.com>
2024-10-30 09:08:55 +0100

    install

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,13 +1,13 @@
 { pkgs, ... }:
 
 {
-  fonts.packagest = with pkgs; [
+  fonts.packages = with pkgs; [
     font-awesome 
     jetbrains-mono
     noto-fonts
     noto-fonts-cjk
     noto-fonts-emoji
 
-    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+    (nerdfonts.override { fonts = [ "JetBrainsMono", "Noto" ]; })
   ];
 }
