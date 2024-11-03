47b1f6cd0df1dc81f3dc9af05b5e0c0fa02fc4a0
santiago <sapalacio2@gmail.com>
2024-11-03 01:34:42 +0100

    new home structure

--- home/programs/hyprland/default.nix
+++ home/programs/hyprland/default.nix
@@ -7,12 +7,11 @@
 
   home.packages = with pkgs; [ 
     waybar
-    swww
+    # swww
   ];
   
-  #test later systemd.user.targets.hyprland-session.Unit.Wants = [ "xdg-desktop-autostart.target" ];
   wayland.windowManager.hyprland = {
     enable = true;
-    systemdIntegration = true;
+    systemd.enable = true;
   };
 }
