ad2a67a062973c3883a076286f09960833b932e5
santiago <sapalacio2@gmail.com>
2024-11-04 22:29:46 +0100

    clean up

--- flake.nix
+++ flake.nix
@@ -3,12 +3,21 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     hyprland.url = "github:hyprwm/Hyprland";
+
+    sops-nix = {
+      url = "github:mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    }
+
   };
 
   outputs = { nixpkgs, nixpkgs-unstable, home-manager, hyprland, ... }@inputs:
