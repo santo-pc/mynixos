de6d47225d12e194a4ce4c4be458438d4f755a4b
santiago <sapalacio2@gmail.com>
2024-11-06 22:47:15 +0100

    display manager changes

--- home/home.nix
+++ home/home.nix
@@ -163,6 +163,28 @@
          );
   };
 
+
+  let configDir = ./config/;
+  in
+  {
+    home.file = {
+      ".config/i3".source = "${configDir}/i3";
+      # ".config/nvim".source = "${configDir}/nvim";
+      # ".config/wallpapers".source = "${configDir}/wallpapers";
+      # ".config/kitty".source = "${configDir}/kitty";
+      # ".config/neofetch".source = "${configDir}/neofetch";
+      # ".config/hypr".source = "${configDir}/hypr";
+      # ".config/swayidle".source = "${configDir}/swayidle";
+      # ".config/swaylock".source = "${configDir}/swaylock";
+      # ".config/wlogout".source = "${configDir}/wlogout";
+      # ".config/waybar".source = "${configDir}/waybar";
+      # ".config/btop".source = "${configDir}/btop";
+      # ".config/wofi".source = "${configDir}/wofi";
+      # ".config/mako".source = "${configDir}/mako";
+    };
+  }
+
+
   programs.home-manager.enable = true;
   home.stateVersion = "22.11";
 
