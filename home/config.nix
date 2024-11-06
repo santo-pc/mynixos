b3e0a1816987b3f9f8752866bce04f1b33d0fa7e
santiago <sapalacio2@gmail.com>
2024-11-06 23:13:58 +0100

    integrate dotfiles to homemanager

--- home/config.nix
+++ home/config.nix
@@ -1,15 +1,15 @@
 let 
-  configDir = ./config;
+  configDir = .config;
 in
 {
   home.file = {
-    ".config/alacritty".source = "${configDir}/alacritty/./config/alacritty";
-    ".config/i3".source = "${configDir}/i3/./config/i3";
-    ".config/kitty".source = "${configDir}/kitty/./config/kitty";
-    ".config/nvim".source = "${configDir}/nvim/./config/nvim";
-    ".config/picom".source = "${configDir}/picom/./config/picom";
-    ".config/polybar".source = "${configDir}/polybar/./config/polybar";
-    ".config/ranger".source = "${configDir}/ranger/./config/ranger";
-    ".config/tmux".source = "${configDir}/tmux/./config/tmux";
+    ".config/alacritty".source = "${configDir}/alacritty/.config/alacritty";
+    ".config/i3".source = "${configDir}/i3/.config/i3";
+    ".config/kitty".source = "${configDir}/kitty/.config/kitty";
+    ".config/nvim".source = "${configDir}/nvim/.config/nvim";
+    ".config/picom".source = "${configDir}/picom/.config/picom";
+    ".config/polybar".source = "${configDir}/polybar/.config/polybar";
+    ".config/ranger".source = "${configDir}/ranger/.config/ranger";
+    ".config/tmux".source = "${configDir}/tmux/.config/tmux";
   };
 }
