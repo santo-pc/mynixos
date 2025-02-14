af36135634267b466b5f509a0a30f942b5e210d0
santiago <sapalacio2@gmail.com>
2025-02-14 23:14:30 +0100

    clean up

--- home/config.nix
+++ home/config.nix
@@ -3,7 +3,7 @@ let
 in
 {
   home.file = {
-    ".config/alacritty".source = "${configDir}/alacritty/.config/alacritty";
+    # ".config/alacritty".source = "${configDir}/alacritty/.config/alacritty";
     ".config/i3".source = "${configDir}/i3";
     ".config/hypr".source = "${configDir}/hypr";
   };
