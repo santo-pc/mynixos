12b6499ce7b62e5bc1ddfbef01064d947cf7b07a
santiago palacio <sapalacio2@gmail.com>
2025-02-26 01:00:36 +0100

    clean up

--- home/config.nix
+++ home/config.nix
@@ -1,14 +1,20 @@
+{ config, ... }:
 let 
   configDir = ./config;
 in
 {
   home.file = {
-    ".config/nvim".source = "${configDir}/nvim";
+    ".config/nvim"= {
+      enable = true;
+      recursive = true;
+      source = config.lib.file.mkOutOfStoreSymlink "${configDir}/nvim";
+    };
+
+    # ".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "${configDir}/nvim";
     ".config/nvim-kickstart".source = "${configDir}/nvim-kickstart";
     ".config/alacritty".source = "${configDir}/alacritty";
     ".config/i3".source = "${configDir}/i3";
     ".config/hypr".source = "${configDir}/hypr";
-    #".config/tmux".source = "${configDir}/tmux";
     ".config/waybar".source = "${configDir}/waybar";
     ".config/wofi".source = "${configDir}/wofi";
   };
